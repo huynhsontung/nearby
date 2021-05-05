d155fe8b8c2164d7b98ff398866ac8e79bd3c4ec
hai007 <hais@google.com>
2021-05-05 08:58:29 -0700

    Internal change
    PiperOrigin-RevId: 372136451

--- cpp/platform/impl/windows/atomic_boolean.h
+++ cpp/platform/impl/windows/atomic_boolean.h
@@ -15,6 +15,7 @@
 #ifndef PLATFORM_IMPL_WINDOWS_ATOMIC_BOOLEAN_H_
 #define PLATFORM_IMPL_WINDOWS_ATOMIC_BOOLEAN_H_
 
+#include <atomic>
 #include "platform/api/atomic_boolean.h"
 
 namespace location {
@@ -30,14 +31,21 @@ class AtomicBoolean : public api::AtomicBoolean {
   // Atomically read and return current value.
   bool Get() const override {
     // TODO(b/184975123): replace with real implementation.
-    return false;
+    return atomicBoolean_;
   };
 
   // Atomically exchange original value with a new one. Return previous value.
   bool Set(bool value) override {
     // TODO(b/184975123): replace with real implementation.
-    return false;
+    bool original = atomicBoolean_;
+
+    atomicBoolean_ = value;
+
+    return original;
   };
+
+ private:
+  std::atomic_bool atomicBoolean_;
 };
 
 }  // namespace windows
