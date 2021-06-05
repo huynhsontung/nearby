7c3e10f03ff9d93e6d2d296f010131553170c010
Tung Huynh <tung75605@gmail.com>
2021-06-05 02:00:20 -0700

    add pch for bazel build

--- cpp/platform/impl/windows/platform.cc
+++ cpp/platform/impl/windows/platform.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "pch.h"
 #include "platform/api/platform.h"
 
 #include "platform/impl/shared/file.h"
